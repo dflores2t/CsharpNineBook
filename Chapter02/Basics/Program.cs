f11325e26faa886114f0e30203903a1192c0014f
Daniel Flores <dflores2t@gmail.com>
2022-09-28 16:48:43 -0600

    csharp nine book

---
+++ Chapter02/Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Basics
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Data.DataSet ds;
+      System.Net.Http.HttpClient client;
+      Console.WriteLine("Hello World!");
+      foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+      {
+        //load the assembly so we can read its details
+        var a = Assembly.Load(new AssemblyName(r.FullName));
+        //declare a variable to count the numbre of methods
+        int methodCount = 0;
+        //loo through all the types in the assembly
+        foreach (var t in a.DefinedTypes)
+        {
+          //add up the counts of methods
+          methodCount += t.GetMethods().Count();
+        }
+        //output the count of types and their methods
+        Console.WriteLine("{0:N0} TYPES WITH {1:N0} METHODS IN {2} ASSEMBLY.", arg0: a.DefinedTypes.Count(), arg1: methodCount, arg2: r.Name);
+      }
+    //let the heighInMetres variable becamoe equal to the value 1.88
+    double heighInMetres = 1.88;
+    Console.WriteLine($"THE VARIABLE {nameof(heighInMetres)} has the value {heighInMetres}");
+    }
+
+  }
+}
---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // unsigned integer means positive whole number
+      // including 0
+      uint naturalNumber = 23;
+      // integer means negative or positive whole number
+      // including 0
+      int integerNumber = -23;
+      // float means single-precision floating point
+      // F suffix makes it a float literal
+      float realNumber = 2.3F;
+      // double means double-precision floating point
+      double anotherRealNumber = 2.3; // double literal
+      int decimalNotation = 2_000_000;
+      int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+      int hexadecimalNotation = 0x_001E_8480;
+      // check the three variables have the same value
+      // both statements output true
+      Console.WriteLine($"{decimalNotation == binaryNotation}");
+      Console.WriteLine(
+      $"{decimalNotation == hexadecimalNotation}");
+
+      Console.WriteLine($"int uses {sizeof(int)} //bytes and can store numbers in the range {int.MinValue:N0}  to {int.MaxValue:N0}.");
+      Console.WriteLine($"double uses {sizeof(double)} //bytes and can storenumbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+      Console.WriteLine($"decimal uses {sizeof(decimal)} // bytes and can storenumbers in the range {decimal.MinValue:N0}to {decimal.MaxValue:N0}.");
+    }
+  }
+}
